fda42a0af0fa52063370306a39282d60a1041ae0
Nazar <rusetskiy2788@mail.ru>
2022-03-02 18:08:16 +0300

    added  part of the realtime-chat functional

--- src/app/components/chat/chat/chat.component.ts
+++ src/app/components/chat/chat/chat.component.ts
@@ -1,6 +1,8 @@
-import {AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewChecked, AfterViewInit, Component, ElementRef, OnInit, SimpleChanges, ViewChild} from '@angular/core';
 import {ChatService} from "../../../services/chat.service";
 import {BaseService} from "../../../services/base-service";
+import {io} from "socket.io-client";
+import {BehaviorSubject} from "rxjs";
 
 
 @Component({
@@ -10,58 +12,83 @@ import {BaseService} from "../../../services/base-service";
 })
 export class ChatComponent implements OnInit , AfterViewInit, AfterViewChecked {
   // newMessage: string;
-  // messageList: string[] = [];
+  newMessage: any;
+  messageList: string[] = [];
+
+  onlineUsers:any[];
+  arrivalMessage: any;
   user: any;
   conversations: any;
   currentOpenChat: any;
   listOfMessages: any;
   sendText: any;
+
+  socket = io('http://localhost:8900');
+  // public  message$: BehaviorSubject<string> = new BehaviorSubject("")
+
   @ViewChild("myDiv") divView: ElementRef;
   @ViewChild('scrollMe') private myScrollContainer: ElementRef;
 
-
-
-
   constructor(private chatService: ChatService, private baseService: BaseService<any>) { }
-
 
   ngOnInit(): void {
     this.baseService.getUser().subscribe((data:any) => {
       this.user = data
       this.chatService.getAllConversation(data._id).subscribe((data: any) => {
         this.conversations = data
+        this.addUserAndGetUsers(data)
       })
     });
 
-    // this.scrollToBottom();
+//как сделать проверку на то что User поменялся?
+    this.socket.on('getMessage', (message) =>{
+      console.log(message);
+      this.arrivalMessage = {
+        sender: message.senderId,
+        text: message.text,
+        createdAt: Date.now(),
+      }
+      console.log(this.arrivalMessage)
+        // this.listOfMessages = [...this.listOfMessages, message];
 
-    // this.chatService.getAllConversation(this.user._id).subscribe((data: any) => {
-    //   console.log(data)
-    // })
-
-    // this.chatService.getNewMessage().subscribe((message: string) => {
-    //   this.messageList.push(message);
-    // })
-  // @ViewChild(this.chatService.socket) soket2: ElementRef;
-  //
-  //   console.log()
+      // this.messageList.push(message);
+      // this.message$.next(message);
+    });
 
   }
 
+  // ngOnChanges(changes: SimpleChanges) {
+  //   console.log(changes)
+  // }
 
   ngAfterViewInit(){
-    // console.log("afterinit");
-    // setTimeout(() => {
-    //   console.log(this.divView);
-    //   this.divView.nativeElement.innerHTML = "Hello Angular 10!";
-    // }, 300);
-    // console.log(this.divView);
-    // this.divView.nativeElement.scrollIntoView({block: "end",behavior: "smooth"});
+    // this.arrivalMessage &&
+    // this.currentOpenChat?.members.includes(this.arrivalMessage.sender) &&
+    // (this.listOfMessages = [...this.listOfMessages, this.arrivalMessage]);
+    // setMessages((prev) => [...prev, this.arrivalMessage]);
+  }
+
+  addArrivalMessage(){
+      this.arrivalMessage &&
+      this.currentOpenChat?.members.includes(this.arrivalMessage.sender) &&
+      (this.listOfMessages = [...this.listOfMessages, this.arrivalMessage]);
   }
 
   ngAfterViewChecked() {
-    // this.scrollToBottom();
   }
+
+  public sendMessage() {
+    this.socket.emit('message', this.newMessage);
+    this.newMessage = '';
+  }
+
+  // public getNewMessage = () => {
+  //   this.socket.on('message', (message) =>{
+  //     this.messageList.push(message);
+  //     // this.message$.next(message);
+  //   });
+  //    //return this.message$.asObservable();
+  // };
 
   openConversation(conversation){
     this.currentOpenChat = conversation
@@ -70,14 +97,37 @@ export class ChatComponent implements OnInit , AfterViewInit, AfterViewChecked {
     })
   }
 
-  handleSubmit(){
+  addUserAndGetUsers(data){
+    console.log("Data",data)
+    this.socket.emit("addUser", data[0]._id);
+    this.socket.on("getUsers", (users) => {
+      this.onlineUsers = this.user.followings.filter((f) => users.some((u) => u.userId === f))
 
+    });
+  }
+
+  soketSendMessage(){
+    const receiverId = this.currentOpenChat.members.find(
+      (member) => member !== this.user._id
+    );
+
+    this.socket.emit("sendMessage", {
+      senderId: this.user._id,
+      receiverId,
+      text:  this.sendText,
+    });
+  }
+
+  handleSubmit(){
     const message = {
       sender: this.user._id,
       text: this.sendText,
       conversationId: this.currentOpenChat._id
     }
+    this.soketSendMessage()
+
     this.sendText = "";
+
     try {
       this.chatService.addMessage(message).subscribe((data: any) => {
         this.listOfMessages = [...this.listOfMessages, data]
@@ -87,21 +137,15 @@ export class ChatComponent implements OnInit , AfterViewInit, AfterViewChecked {
     } catch (err) {
       console.log(err)}
   }
-
   // scrollToBottom(): void {
   //   try {
   //     this.myScrollContainer.nativeElement.scrollTop = this.myScrollContainer.nativeElement.scrollHeight;
   //   } catch(err) { }
   // }
 
-
-
-
-
-
-  sendMessage() {
-    // this.chatService.sendMessage(this.newMessage);
-    // this.newMessage = '';
-  }
+  // sendMessage() {
+  //   this.chatService.sendMessage(this.newMessage);
+  //   this.newMessage = '';
+  // }
 
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -57,19 +57,19 @@ export class ChatService {
     );
   }
 
-  socket = io('http://localhost:8900');
-
-  public sendMessage(message) {
-    this.socket.emit('message', message);
-  }
-
-  public getNewMessage = () => {
-    this.socket.on('message', (message) =>{
-      this.message$.next(message);
-    });
-
-    return this.message$.asObservable();
-  };
+  // socket = io('http://localhost:8900');
+  //
+  // public sendMessage(message) {
+  //   this.socket.emit('message', message);
+  // }
+  //
+  // public getNewMessage = () => {
+  //   this.socket.on('message', (message) =>{
+  //     this.message$.next(message);
+  //   });
+  //
+  //   return this.message$.asObservable();
+  // };
 
 
 }
